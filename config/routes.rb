b1d9ea77db858bf1d129ce0d7fb30b5e47dfa56b
ryamakuchi <r.yamakuchi@gmail.com>
2019-08-12 15:20:26 +0900

    letter_opener の routes 設定を修正した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
