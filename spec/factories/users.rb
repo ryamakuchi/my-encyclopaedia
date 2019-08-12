a3c11f1520e98dcce7464deb913ab0f7899303e3
ryamakuchi <r.yamakuchi@gmail.com>
2019-08-12 20:31:32 +0900

    Faker を導入した

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    password = Faker::Internet.password(8)
+    email { Faker::Internet.free_email }
+    password { password }
+    password_confirmation { password }
   end
 end
