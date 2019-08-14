c45840dec1d6d2ed904ef3972895f3a9eabd9769
ryamakuchi <r.yamakuchi@gmail.com>
2019-08-14 13:37:15 +0900

    system spec を少し修正した

--- spec/system/user_registration_spec.rb
+++ spec/system/user_registration_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'POST devise/registrations#create', type: :system do
   scenario 'アカウント登録が成功する' do
-    # visit new_user_registration_path
-    visit('/users/sign_up')
+    visit new_user_registration_path
 
     password = Faker::Internet.password(min_length: 6)
 
@@ -14,11 +13,7 @@ RSpec.describe 'POST devise/registrations#create', type: :system do
     click_on "アカウント登録"
 
     expect(page).to have_content '本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。'
-  end
-
-  context 'Eメールとパスワード（6字以上）とパスワード（確認用）が正しく入力されているとき' do
-    it '本人確認用のメールが送信される'
-    it 'トップページが表示されている'
+    
   end
 
   describe 'バリデーションエラーの場合' do
