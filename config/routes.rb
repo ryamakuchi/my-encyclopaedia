052dfd37206b8cb27873ad1804872641a04f18e5
ryamakuchi <r.yamakuchi@gmail.com>
2019-08-16 09:08:19 +0900

    一旦情報が更新できることを確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   root 'home#index'
+
+  # devise_for :users, skip: [:registrations], controllers: {
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
+  # devise_scope :user do
+  #   get 'users/sign_up' => 'users/registrations#new', as: :new_user_registration
+  #   get 'users/edit' => 'users/registrations#edit', as: :edit_user_registration
+  #   # get 'users/cancel' => 'users/registrations#cancel', as: :cancel_user_registration
+  #   post 'users' => 'users/registrations#create', as: :user_registration
+  #   put 'users/edit' => 'users/registrations#update', as: :update_user_registration
+  #   patch 'users/edit' => 'users/registrations#update_password', as: :update_user_password
+  #   delete 'users/edit' => 'users/registrations#destroy', as: :destroy_user_registration
+  # end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
